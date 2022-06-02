8740626237ca912bdbc9023e7c9d09127e5ffd52
Hazrul <hal1993@hotmail.com>
2022-06-02 18:51:34 +0800

    banner added

--- Components/Banner.tsx
+++ Components/Banner.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div>
+        <div className='banner-content h-80 relative md:h-96 lg:h-[90vh]'>
+            <div className='absolute left-5 bottom-8'>
+                <h3 className='text-4xl'>Le Catering</h3>
+            </div>
+        </div>
+    </div>
   )
 }
 
